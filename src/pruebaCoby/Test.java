181fc9188e2435f19fe766a5534027ed4cf9cecb
vceballos1978 <vladimir.ceballos@gmail.com>
2020-11-19 19:30:56 -0500

    practice switch

---
+++ src/pruebaCoby/Test.java
@@ -0,0 +1,25 @@
+package pruebaCoby;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		
+		switch (x) {
+		  case 1:
+		    System.out.println("Uno");
+		    break;
+		  case 2:
+		    System.out.println("Dos");
+		    break;
+		  case 3:
+		    System.out.println("Tres");
+		    break;
+		  default:
+		    System.out.println("Otro número");
+		}
+
+	}
+
+}
